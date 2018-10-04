066187f806f87c20b7b0675501f53bda6bddd072
Alberto Puche <alberto.puche@emdisalud.com.co>
2018-10-04 15:01:22 -0500

    Commit inicial

---
+++ src/api/config.js
@@ -0,0 +1,14 @@
+export default {
+    appName: 'musicplatzi',
+    apiKey: '21bd02c1638fba251863cb88f565859d',
+    secret: '4c03aac4588f058b43aa5bd6088fb4f9',
+    registeredTo: 'julian_garces'
+}
+
+/*
+https://musicplatzi.com/callback
+Application name  => musicplatzi
+API key => 21bd02c1638fba251863cb88f565859d
+Shared secret => 4c03aac4588f058b43aa5bd6088fb4f9
+Registered to => julian_garces
+*/
